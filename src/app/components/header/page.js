8ed4bbc4161724e423782d4ac75c1df57500e952
alizey121 <alizey121@gmail.com>
2024-10-07 18:36:27 +0500

    "card"

--- src/app/components/header/page.js
+++ src/app/components/header/page.js
@@ -1,16 +1,67 @@
 import Image from "next/image";
-import logo from "../../../../public/assets/images/dominos-logo.png";
+import logo from "../../../../public/assets/images/Main-logo.png";
 import Link from "next/link";
+import { VscAccount } from "react-icons/vsc";
+import { BsCart3 } from "react-icons/bs";
 export const Header = () => {
   return (
     <>
-      <div class="bg-slate-700 text-white flex flex-wrap justify-around">
-        <div class="">
-          <Image src={logo} width={100} height={100} alt="logo" />
+      <div className="h-20 w-full ">
+        <div class="bg-white text-black  grid grid-cols-3 gap-10 ">
+          <div class="flex justify-around flex-wrap">
+            <Link href="#">
+              {" "}
+              <div className="flex flex-wrap">
+                <Image src={logo} width={50} height={50} alt="logo" />
+                <span className="text-blue-900 text-2xl font-bold content-center">
+                  Domino&apos;s
+                </span>
+              </div>{" "}
+            </Link>
+            {/* <div className="">
+          <h1>Delievering to </h1>
+          <h1 className="font-bold">please select location</h1>
+        </div> */}
+          </div>
+
+          <div className="justify-center mx-4 px-5 gap-5 content-center">
+            <nav className=" w-full">
+              <ul className="flex flex-wrap max-w-full gap-5 font-bold text-lg">
+                <Link href="#">
+                  <li className="text-md ">MENU</li>
+                </Link>
+                <Link href="#">
+                  <li className="text-md ">DEALS</li>
+                </Link>
+                <Link href="#">
+                  <li className="text-md ">STORES </li>
+                </Link>
+                <Link href="#">
+                  <li className="text-md ">ALLIANCES</li>
+                </Link>
+              </ul>
+            </nav>
+          </div>
+
+          <div className=" flex flex-wrap w-96  justify-around">
+            <div>
+              <BsCart3
+                style={{ width: "40px", height: "40px", color: "blue" }}
+                className="justify-center m-2 items-center content-center"
+              />
+            </div>
+            <div className="flex flex-wrap justify-around">
+              <VscAccount
+                style={{ width: "30px", height: "30px" }}
+                className="justify-center m-2 items-center content-center"
+              />
+              <span className="font-bold justify-center items-center content-center">
+                My Account
+              </span>
+            </div>
+          </div>
         </div>
-        <div class="">
-          <Link href="#">Login</Link>| <Link href="#">Register</Link>
-        </div>
+        <hr />
       </div>
     </>
   );
--- src/app/pages/indexPage/page.js
+++ src/app/pages/indexPage/page.js
@@ -1,13 +1,29 @@
 import { Footer } from "@/app/components/footer/page";
 import { Header } from "@/app/components/header/page";
-
+import { RiEBike2Line } from "react-icons/ri";
+import { GiCardPickup } from "react-icons/gi";
+import Card from "../../components/cards/page";
 export default function Home() {
   return (
     <>
-      <div class="min-h-screen">
+      <div class="min-h-screen bg-slate-900">
         <Header />
-        <h1 class="">Main</h1>
-        <Footer />
+        <div className="flex flex-wrap rounded-md justify-center content-center items-center">
+          <div className="bg-sky-600 py-2 text-white flex flex-wrap  justify-around w-96 rounded-l-lg h-16 ">
+            <RiEBike2Line style={{ width: "40px", height: "40px" }} />
+            <h1 className="text-2xl font-bold">Delivery </h1>
+          </div>
+          <span className="bg-white absolute text-black rounded-full w-10 h-10  items-center content-center font-bold ">
+            OR
+          </span>
+          <div className="bg-red-600  text-white flex flex-wrap  w-96 rounded-r-lg h-16 justify-around items-center content-center place-content-center ">
+            <GiCardPickup style={{ width: "40px", height: "40px" }} />
+            <h1 className="text-2xl font-bold">Pick Up</h1>
+          </div>
+        </div>
+<Card/>
+        
+        {/* <Footer /> */}
       </div>
     </>
   );
